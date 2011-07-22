3141fc56e181bfedfe6d81ff312374292ea6c47e
Ryan Done <rbdone@gmail.com>
2011-07-22 08:13:07 -0600

    Great commit.

--- src/com/pickledego/weatherdemo/MainActivity.java
+++ src/com/pickledego/weatherdemo/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends Activity
                 Log.d(TAG, "Clicked Button");
                 String zipCode = ((EditText)findViewById(R.id.main_zipCode)).getText().toString();
                 callWeatherAPI(zipCode);
+                
             }
         });
     }
